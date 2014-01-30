8bfaef852f2cf8638366159ec3af623c472c72ac
spiritworld <markku.painomaa@lut.fi>
2014-01-30 14:16:44 +0200

    asdf

--- Client/timer.c
+++ Client/timer.c
@@ -1,69 +1,25 @@
+/*
+* CT30A5002 Games and Networking 2013-14
+* Authors:
+*   Arttu Hanska (0342181) -  arttu.hanska@lut.fi
+*   Joonas Blomberg (0342152) - joonas.blomberg@lut.fi
+*   Markku Painomaa (0358551) - markku.painomaa@lut.fi
+*/
+
 #include "SDL/SDL.h"
 #include "timer.h"
 
-void startTimer() {
-    //Start the timer
-    started = 1;
-
-    //Unpause the timer
-    paused = 0;
-
-    //Get the current clock time
-    startTicks = SDL_GetTicks();
+void init_timer() {
+	start_ticks = SDL_GetTicks();
 }
 
-void stopTimer() {
-    //Stop the timer
-    started = 0;
+/* delta time */
+int delta_time() {
 
-    //Unpause the timer
-    paused = 0;
+	if(!start_ticks)
+		return SDL_GetTicks() - start_ticks;
+	else
+		return 0;
+
 }
 
-int get_ticks() {
-    //If the timer is running
-    if(started)
-    {
-        //If the timer is paused
-        if(paused)
-        {
-            //Return the number of ticks when the timer was paused
-            return pausedTicks;
-        }
-        else
-        {
-            //Return the current time minus the start time
-            return SDL_GetTicks() - startTicks;
-        }
-    }
-
-    //If the timer isn't running
-    return 0;
-}
-
-void pauseTimer(){
-    //If the timer is running and isn't already paused
-    if(started && !paused)
-    {
-        //Pause the timer
-        paused = 1;
-
-        //Calculate the paused ticks
-        pausedTicks = SDL_GetTicks() - startTicks;
-    }
-}
-
-void unpauseTimer(){
-    //If the timer is paused
-    if(paused)
-    {
-        //Unpause the timer
-        paused = 0;
-
-        //Reset the starting ticks
-        startTicks = SDL_GetTicks() - pausedTicks;
-
-        //Reset the paused ticks
-        pausedTicks = 0;
-    }
-}
