4ea812e8aee766332b02d994405069520ebded54
Arttu Hanska <arttu.hanska@gmail.com>
2014-02-10 20:03:11 +0200

    Functions to create structs

--- gameLogic.c
+++ gameLogic.c
@@ -6,6 +6,268 @@
 *   Markku Painomaa (0358551) - markku.painomaa@lut.fi
 */
 
-#include <stdlib.h>
+#include "gameLogic.h"
+
+int startGame(game* gameState){
+
+    while(1){
+        // Main game logic loop
+
+        // Update all player character locations
+        //updatePlayerLocation();
+        
+        // Update all enemy locations
+        //updateEnemyLocation();
+        
+        // Have player character's shot and do they hit enemies
+        //checkHit();
+        
+        // Are player character and enemy colliding
+        //checkCollision();
+        
+        // Check end condition
+        //checkEnd();
+        
+        // Relay chat message to correct clients
+        //relayChat();
+        
+        // Send game state to all clients
+        //sendGameState();
+        
+        // Framecap - implement without SDL_Delay
+        /*if(delta_time() < 1000/FPS) {
+            SDL_Delay((1000/FPS) - delta_time());
+        } */
+    }
+
+    return 0;
+}
+
+// Update PC location
+void updatePlayerLocation (void){
 
 
+}
+
+// Update enemy location
+void updateEnemyLocation (void){
+
+
+}
+
+// Determine if enemy is shot - if PC has shot check if there are enemies on firing line
+void checkHit (void){
+
+
+}
+
+// Function to determine if player character and enemy collide
+void checkCollision (void){
+
+
+}
+
+
+// Check end condition: all PCs are dead or enemyLimit and enemyCount are zero
+void checkEnd (void){
+
+
+}
+
+// Relay chat messages to all clients
+void relayChat (void){ // Check if private message
+
+
+}
+
+// New game state
+game* newGame(void) {
+
+    // Allocate game struct
+    game *gameState = (game*)malloc(sizeof(game));
+
+    // Set the game state
+    //gameState->playerList = NULL; // Clear player list
+    //gameState->enemyList = NULL; // Clear enemy list
+    gameState->playerCount = 1; // Number of players after the host joins
+    gameState->enemyCount = 0; // Number of enemies when game is created
+    gameState->levelNumber = 1; // Number of the game level (1-3), new game starts from 1
+    gameState->enemyLimit = 10; // Enemy spawn limit for level 0
+    gameState->enemySpawnRate = 1; // Enemy spawn limit for level 1
+    gameState->enemyBaseSpeed = 1; // Enemy base speed for level 1
+    gameState->currentState = 0; // 0: Waiting game, 1: inLobby, 2: inGame
+
+    return gameState;
+}
+
+// Free game state
+void freeGame(game* gameState) {
+    if(gameState) {
+        free(gameState);
+    }
+}
+
+game* addPlayer(game* gameState, int isHost, int playerNumber, char* playerName){
+
+    // Allocate new player struct
+    player *newPlayer = (player*)malloc(sizeof(player));
+    
+    // Set new player parameters
+    newPlayer->xcoord = 0 ; // Initial X coordinate
+    newPlayer->ycoord = 0; // Initial Y coordinate
+    newPlayer->viewDirection = 0; // Initial direction the PC is facing
+    newPlayer->health = 3; // Initial Health points
+    newPlayer->hasShot = 0; // Player has not shot
+    newPlayer->isHost = isHost; // Is the player the host - 0: False, 1: True
+    newPlayer->playerNumber = playerNumber; // Player's number
+    newPlayer->playerName = playerName; // Player's name
+    
+    //memset(gameState->playerList,newPlayer,sizeof(newPlayer));
+    //gameState->playerList[newPlayer->playerNumber] = newPlayer; // Allocate new player
+    
+    return gameState;
+}
+
+int main (void){
+
+    game* gameState = NULL;
+
+    // Game state for new game
+    gameState = newGame();  
+
+    // Change to inLobby state
+    gameState->currentState = 1;
+
+    
+    // Check if in lobby state
+    if(gameState->currentState == 1){
+    
+        // Waiting in lobby for players and game start
+        while(1){
+        
+            // Clean temporary variable
+            player* newPlayer = NULL;
+            
+            // Add player to game
+            //addPlayer(game* gameState, int isHost, int playerNumber, char playerName[16]);
+            gameState = addPlayer(gameState, 1, 0, "asd");
+            
+            // Start the game when host is ready
+            //if(isHostReady() == 1){
+            //    break;
+            //}
+            
+            // Wait 2 seconds before retry
+            sleep(2);
+        }
+    }
+
+     
+     
+     
+    // Start the game
+    startGame(gameState);
+    
+    // Cleanup after the game ends
+    freeGame(gameState);
+    
+    return 0;
+}
+
+
+// Go to lobby state
+void startLobby(game* gameState){
+
+
+}
+
+/*
+
+
+void move_enemies() {
+
+if(enemy->health <= 0)
+return;
+
+int x1, y1, x2, y2;
+
+x1 = player.b.x;
+y1 = player.b.y;
+
+x2 = enemy->b.x;
+y2 = enemy->b.y;
+
+//calculate x distance to player, move towards
+if((x1 - x2) < 0) {
+enemy->dir = LEFT;
+enemy->b.x -= EN_VEL;
+}
+else {
+enemy->dir = RIGHT;
+enemy->b.x += EN_VEL;
+}
+//calculate y distance, move towards
+if((y1 - y2) < 0){
+enemy->dir = UP;
+enemy->b.y -= EN_VEL;
+}
+else {
+enemy->dir = DOWN;
+enemy->b.y += EN_VEL;
+}
+}
+
+void move_player() {
+
+//left/right
+player.b.x += player.xVel;
+
+//screen left/right border
+if((player.b.x < 0) || (player.b.x + PC_DIMS > SCREEN_WIDTH)) {
+//move back
+player.b.x -= player.xVel;
+}
+
+// up/down
+player.b.y += player.yVel;
+
+//screen top/bottom border
+if((player.b.y < 0) || ( player.b.y + PC_DIMS > SCREEN_HEIGHT)) {
+//move back
+player.b.y -= player.yVel;
+}
+}
+
+int check_collision(SDL_Rect A, SDL_Rect B) {
+
+    int leftA, rightA, topA, bottomA;
+    int leftB, rightB, topB, bottomB;
+
+    //sides of rect A
+    leftA = A.x;
+    rightA = A.x + A.w;
+    topA = A.y;
+    bottomA = A.y + A.h;
+
+    //sides of rect B
+    leftB = B.x;
+    rightB = B.x + B.w;
+    topB = B.y;
+    bottomB = B.y + B.h;
+
+    //if any of the sides from A are outside of B
+    if(bottomA <= topB)
+        return 0;
+
+    if(topA >= bottomB)
+        return 0;
+
+    if(rightA <= leftB)
+        return 0;
+
+    if(leftA >= rightB)
+        return 0;
+
+    //if none of the sides from A are outside B
+    return 1;
+} */
--- gameLogic.h
+++ gameLogic.h
@@ -7,50 +7,81 @@
 */
 
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800
+
+#define COOLDOWN 0.5 // Cooldown for shooting
 
 // Enemy struct
 typedef struct t_enemy {
-    int xcoord; // X coordinate
-    int ycoord; // Y coordinate
+    int xcoord; // X coordinate, upper left corner
+    int ycoord; // Y coordinate, upper left corner
     int viewDirection; // Direction the enemy is facing and moving
-    int health; // Enemy health - 0: Uninjured, 1: Injured, 2: Dead
-    int following // ID of the player the enemy is following
+    uint8_t health; // Enemy health - 0: Uninjured, 1: Injured, 2: Dead
+    int following; // ID of the player the enemy is following
 } enemy;
 
 // Player character stuct 
 typedef struct t_player {
-    int xcoord; // X coordinate
-    int ycoord; // Y coordinate
+    int xcoord; // X coordinate, upper left corner
+    int ycoord; // Y coordinate, upper left corner
     int viewDirection; // Direction the PC is facing, 0-359 degrees
     int health; // Three Health points, third hit kills
-    int hasShot; // Has the player shot after sending the last clientState
+    uint8_t hasShot; // Has the player shot after sending the last clientState
     int isHost; // Is the player the host - 0: False, 1: True
+    int playerNumber; // Player's number
+    char* playerName; // Player's name
 } player;
 
 // Game struct
 typedef struct t_game {
     player playerList; // List of PCs in game
+    enemy enemyList; // List of enemies in game
     int playerCount; // Number of players in game
     int enemyCount; // Number of enemies in game
     int levelNumber; // Number of the game level, defines level parameters
     int enemyLimit; // How many enemies current level will spawn
     int enemySpawnRate; // How quickly new enemies are spawned
     int enemyBaseSpeed; // Base speed for enemies
-    int gameState; // 0: Waiting game, 1: inLobby, 2: inGame
+    int currentState; // 0: Waiting game, 1: inLobby, 2: inGame
 } game;
 
 
 // Function to update enemy location
-void updateEnemyLocation (int, int);
+void updateEnemyLocation (void);
 
 // Function to update PC location
-void updatePlayerLocation (int);
+void updatePlayerLocation (void);
 
 // Funtion to determine if enemy is shot - if PC has shot check if there are enemies on firing line
-void checkHit (player*);
+void checkHit (void);
 
 // Function to determine if player character and enemy collide
-void checkCollision (player*, enemy*);
+void checkCollision (void);
 
 // Check end condition: all PCs are dead or enemyLimit and enemyCount are zero
-int checkEnd (void);
+void checkEnd (void);
+
+// Relay chat messages to all clients
+void relayChat (void); // Check if private message
+
+// Send game state to all clients
+void sendGameState(void);
+
+// New game struct
+// Params:
+game* newGame(void);
+void freeGame(game*);
+
+// Start new game
+int startGame(game*);
+
+// Go to lobby state
+void startLobby(game*);
+
+// Add new player to existing game lobby
+// Params: gameState struct, isHost, playerNumber, playerName
+game* addPlayer(game*, int, int, char[16]);
